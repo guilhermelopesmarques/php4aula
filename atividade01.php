9a039e3b4eefc19f832d788d402b00f2e9608b26
guilherme <112526329+guilhermelopesmarques@users.noreply.github.com>
2023-05-25 22:19:03 -0300

    terminado

---
+++ atividade01.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Atividade01</title>
+</head>
+<body>
+    <form method="post" action="resultado.php">
+        <input type="number" name= "n1">
+        <input type="number" name= "n2">
+        <button type="submit">Enviar</button>
+</form>
+</body>
+</html>
---
+++ resultado.php
@@ -0,0 +1,7 @@
+<?php
+$n1 = $_POST["n1"];
+$n2 = $_POST["n2"];
+echo $n1+$n2;
+?>
+<br>
+<a href="atividade01.php">Voltar</a>
